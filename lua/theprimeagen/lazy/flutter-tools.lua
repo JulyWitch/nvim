2f22f3867c977b0cfa8cd0854b7514efbd782994
Sajad Abdollahi <sajad.abdollahi1380@gmail.com>
2024-12-22 21:40:06 +0330

    ✨ Add flutter stuff

--- lua/theprimeagen/lazy/flutter-tools.lua
+++ lua/theprimeagen/lazy/flutter-tools.lua
@@ -8,10 +8,62 @@ return {
     },
     config = function()
         require("flutter-tools").setup {
+            fvm = false,
+            widget_guides = { enabled = true },
+            closing_tags = {
+                enabled = true,
+                highlight = 'LineNr',
+                prefix = '󰡒  ',
+                priority = 0,
+            },
+            dev_log = {
+                enabled = true,
+                notify_errors = false,
+                filter = function(log_line)
+                    if log_line:find('ImpellerValidationBreak') then
+                        return false
+                    end
+                    return true
+                end,
+            },
+            decorations = {
+                statusline = {
+                    app_version = true,
+                    device = true,
+                    project_config = true,
+                }
+            },
+            lsp = {
+                settings = {
+                    showTodos = true,
+                    completeFunctionCalls = true,
+                    analysisExcludedFolders = {
+                        vim.fn.expand('$HOME/.pub-cache'),
+                        vim.fn.expand('$HOME/development'),
+                    },
+                    renameFilesWithClasses = "prompt",
+                    enableSnippets = true,
+                    updateImportsOnRename = true,
+                },
+            },
             debugger = {
                 enabled = true,
-                register_configurations = function(_)
-                    require("dap").configurations.dart = {}
+                run_via_dap = true,
+                exception_breakpoints = {},
+                evaluate_to_string_in_debug_views = true,
+                register_configurations = function()
+                    local dap = require("dap")
+                    dap.adapters.dart = {
+                        type = "executable",
+                        command = "/Users/sajad/development/flutter/bin/flutter",
+                        args = { "debug-adapter" },
+                    }
+                    dap.configurations.dart = {
+                        {
+                            request = "launch",
+                            type = "dart"
+                        }
+                    }
                     require("dap.ext.vscode").load_launchjs()
                 end,
             },
