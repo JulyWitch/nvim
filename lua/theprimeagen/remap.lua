65c672279c1f91d8e51deb73ce16c536017649d1
Sajad Abdollahi <sajad.abdollahi1380@gmail.com>
2024-12-30 14:36:09 +0330

    ✨ Add neoformat and some flutter remaps

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -30,7 +30,9 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", ":Neoformat<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>r", ":FlutterRun<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>R", ":FlutterRestart<CR>", { noremap = true })
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
